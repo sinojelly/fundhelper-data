4dc21c33c6a71020ece02ef5a47b3a1834665e42
Jelly Chen <sinojelly@163.com>
2019-12-15 12:15:50 +0800

    automatically submit.

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 # -- coding: utf-8 --
 
 import os
+import subprocess
+
+
+def run_external_cmd(cmd, msg_in=''):
+    try:
+        proc = subprocess.Popen(cmd,
+                                shell=True,
+                                stdin=subprocess.PIPE,
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                )
+        stdout_value, stderr_value = proc.communicate(msg_in)
+        return stdout_value, stderr_value
+    except ValueError as err:
+        # log("ValueError: %s" % err)
+        return None, None
+    except IOError as err:
+        # log("IOError: %s" % err)
+        return None, None
 
 
 def run_git_submit(work_dir):
@@ -9,11 +28,16 @@ def run_git_submit(work_dir):
     push_cmd = git_cmd + "push"
     status_cmd = git_cmd + "status"
     cmd = commit_cmd + " & " + push_cmd + " & " + status_cmd
-    result = os.popen(cmd).read()
-    print(result)
-    return result
+    # result = os.popen(cmd).read()
+    stdout_value, stderr_value = run_external_cmd(cmd)
+    print(stdout_value)
+    print("---------------err-------------")
+    print(stderr_value)
+    return stdout_value, stderr_value
 
 
 if __name__ == '__main__':
-    run_git_submit("/usr/local/fundhelper-data")
+    run_git_submit("D:\\Develop\\projects\\web-projects\\fundhelper-data")
+    # run_git_submit("/usr/local/fundhelper-data")
+
 
