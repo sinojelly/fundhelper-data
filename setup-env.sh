7ba5ee1a790910bfbbbf4b8ee55d41721f171d12
docker <docker@cloud>
2020-08-01 07:52:26 +0800

    modify config path.

--- setup-env.sh
+++ setup-env.sh
@@ -1,6 +1,6 @@
 chmod a+w .git -R
 git config --local user.name docker
 git config --local user.email docker@cloud
-git config --local credential.helper "store --file /usr/local/fundhelper-data/gitpass"
-echo "https://$1%40gmail.com:$2@github.com" >> gitpass
+git config --local credential.helper "store --file /usr/local/fundhelper/data/.gitpass"
+echo "https://$1%40gmail.com:$2@github.com" >> .gitpass
 
