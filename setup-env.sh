3b1e9012e0c3cd3539999bf5acd0413fa242ed4b
Jelly Chen <sinojelly@163.com>
2020-08-01 08:46:05 +0800

    add git cmd parameter to avoid modify other .git

--- setup-env.sh
+++ setup-env.sh
@@ -1,10 +1,11 @@
 # $1 -- username
 # $2 -- pass
 # $3 -- path
+# $4 -- git cmd
 
-chmod a+w .git -R
-git config --local user.name docker
-git config --local user.email docker@cloud
-git config --local credential.helper "store --file $3/.gitpass"
+chmod a+w $3/.git -R
+$4 config --local user.name docker
+$4 config --local user.email docker@cloud
+$4 config --local credential.helper "store --file $3/.gitpass"
 echo "https://$1%40gmail.com:$2@github.com" > $3/.gitpass
 
